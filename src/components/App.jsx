b7dac3889aa7e5ec4c5e1a7736ffaeceb9df06d8
Ed <silva.email@yahoo.com>
2024-03-02 19:20:49 -0500

    Update styles.css and App.jsx, add functionality to display current time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  
+  const [currentTime, setCurrentTime] = useState(time)
+
+  const showTime = () => {
+    let newTime = new Date().toLocaleTimeString();
+    setCurrentTime(newTime)
+  }
+  console.log(currentTime);  
+
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick = {showTime}>Get Time</button>
     </div>
   );
 }
